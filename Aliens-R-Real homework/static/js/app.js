9834b33bdee031b49f20bd2eec52110582b47fbe
gofish1234 <50524309+gofish1234@users.noreply.github.com>
2019-09-23 20:55:11 -0500

    Add files via upload

--- Aliens-R-Real homework/static/js/app.js
+++ Aliens-R-Real homework/static/js/app.js
@@ -1,34 +1,33 @@
-
 // from data.js
-var tableData = data;
+var myData = data;
+//console.log(tableData);
+var tbody = d3.select("tbody");
 
-tableData.forEach(tableData) => {
-  var row = tbody.append("tr");
- Object.entries(tableData).forEach(([key, value]) => {
-  var cell = row.append("td");
-   cell.text(value);    
-   });
- });
-
-// YOUR CODE HERE!
-// Select the button
+function buildtable(myData) {
+ // var tbody = d3.select("tbody");
+  myData.forEach((ufodata) => {
+    var row = tbody.append("tr");
+    Object.entries(ufodata).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+};
 var button = d3.select("#filter-btn");
-var tbody = d3.select("tbody")
 
-    
-//function getdata (tableData) {    
-// return data.datetime === inputValue)
-//    };
-//var filtereddata = tableData.filter(getdata);
-
-function filter() {
-  var inputDate = d3.select("#datetime");
-  var inputValue = inputDate.property("value");
-  console.log(inputDate);
-  console.log(inputValue);
-  var filteredData = tableData.filter(tableData => tableData.datetime ===inputValue);
+button.on("click", function() {
+  tbody.html("")
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+  var filteredData = myData.filter(myData => myData.datetime === inputValue);
+  filteredData.forEach((ufodata) => {
+//  var tbody = d3.select("tbody");
+  var row = tbody.append("tr");
+  Object.entries(ufodata).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+    console.log(inputValue);
+  });
 });
-  
-
-//create function upon button click
-button.on("click", filter)
+});
+buildtable(myData);
